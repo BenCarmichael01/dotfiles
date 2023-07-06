e4bac7f7805ffb957f5c25f68c70f81113146dfd
Jaques22 <38633150+Jaques22@users.noreply.github.com>
2023-07-06 23:08:22 +0100

    change kitty theme & tweak hypr conf

--- .local/bin/gtk-dark-light-toggle.sh
+++ .local/bin/gtk-dark-light-toggle.sh
@@ -5,9 +5,11 @@ set -euo
 if test "$(gsettings get org.gnome.desktop.interface color-scheme)" = "'prefer-light'"; then
   gsettings set org.gnome.desktop.interface color-scheme prefer-dark
   gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
-  kitty +kitten themes --reload-in=all Catpuccin-Macchiato
+  kitty +kitten themes --reload-in=all Cherry Midnight
+  notify-send "Dark Mode Enabled"
 else
   gsettings set org.gnome.desktop.interface color-scheme prefer-light
   gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita'
-  kitty +kitten themes --reload-in=all Catpuccin-Latte
+  kitty +kitten themes --reload-in=all Cherry
+  notify-send "Light Mode Enabled"
 fi
